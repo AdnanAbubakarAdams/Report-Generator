00619dd77230f9683f23218e8b8d6a7789ad6cc0
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-04-11 18:18:28 -0400

    line chart for dailydeposit completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
+import { useState } from 'react';
+import { AccountingDailyDepositData } from "./components/AccountingDailyDepositData";
 import BarChat from './components/BarChat';
-// import LineChart from './components/LineChart';
+import LineChart from './components/LineChart';
 
 function App() {
+
+  const [accountData, setAccounData] = useState({
+    labels: AccountingDailyDepositData.map((data) => data.id),
+    datasets: [
+      {
+        label: "ACCOUNTING DAILY DEPOSIT",
+        data: AccountingDailyDepositData.map((data) => data.totalDeposit),
+        backgroundColor: ["red", "gold", "green" ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
   return (
     <div className="App">
       <BarChat />
-      {/* <LineChart /> */}
+      <LineChart chartData={accountData}/>
 
     </div>
   );
